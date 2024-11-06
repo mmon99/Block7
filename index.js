a5425c1b1e53c94469c2dffccbde68ca51bf7cb4
Myat Mon <monmdny@gmail.com>
2024-11-05 23:40:00 -0500

    Block7

---
+++ index.js
@@ -0,0 +1,18 @@
+//write alert to customer "You have received this message because you have been chosen to open an important vault"
+//assign firstCode by using plus
+//assign secondCode by using minus
+//assign thirdCode by using mulitply
+//write alert to customer "Here is the secret combination"
+
+alert(`You have received this message because you have been chosen to open an important vault.`)
+
+const firstCode=5+5
+const secondCode=60-20
+const thirdCode=13*3
+
+console.log (firstCode)
+console.log (secondCode)
+console.log (thirdCode)
+
+alert (`Here is the secret combination:`)
+
